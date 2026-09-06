867fdd6451fdb5be02fad68ae69cc142fcc4bec7
nikhil8757301942-cell <nikhil8757301942@gmail.com>
2026-09-06 23:08:09 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "swasth-ai-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+
+        return response ||
+          fetch(event.request);
+
+      })
+  );
+
+});
